e8c98444116fde44c06e6b7e0c3247569a94de67
Naby-04 <smohamed.diop@gmail.com>
2024-10-25 21:55:20 +0100

    initialisation des exos

---
+++ main.js
@@ -0,0 +1,75 @@
+// Exercice1: Inverser les valeurs de deux variables
+// À partir des variables suivantes, faire en sorte d'inverser leur valeur:
+// -Declarer deux variables a et b et affecter leur respectivement ces valeurs: 1 et 5.
+// -Inverser leurs valeurs de telle sorte que les valeurs se presente comme tel: a = 5; b = 1;
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exercice2: Puissance de 2
+// -Realiser un programme permettant à l'utilisateur de saisir la puissance de 2 qu'il souhaite.
+// -Exemple: si l'utilisateur saisit 3, le programme affiche 2^3 = 8;
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exercice3: Afficher une table de multiplication
+// -Demander à l'utilisateur de rentrer la table de multiplication souhaiter.(de 1 à 10)
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exercice4: Saisie clavier
+// -Vous devez realiser un programme de saisir le nom, le prenom et l'age d'un utilisateur.
+// -Le programme affiche ensuite le message suivant: votre nom est:"le nom que l'utilisateur à rentré";votre prenom est:"le prenom que l'utilisateur à rentré";vous avez:"l'age que l'utilisateur à rentré"
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exercice5: Utiliser une boucle for pour afficher les nombres de 1 à 10 dans la console.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exercice6: Utiliser une boucle while pour afficher les nombres de 1 à 10 dans la console.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exercice7: Créer un tableau de noms de fruits (au moins 5 noms). Utilisez une boucle for pour afficher chaque nom de fruit dans la console.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo-8: Conditions simples :
+// Écris un programme qui demande un nombre et affiche si ce nombre est positif, négatif ou nul.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo-9: Calcul de l’âge :
+// Déclare une variable pour l’année actuelle et une autre pour ton année de naissance. Calcule et affiche ton âge en utilisant une opération de soustraction.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo 10 : Opérations de base :
+// Demande à l’utilisateur deux nombres et affiche la somme, la différence, le produit, et le quotient des deux.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo 11: Trouver les nombres pairs :
+// Écris une boucle for pour afficher tous les nombres pairs de 1 à 20.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo 12: Conditions avancées :
+// Demande un mot de passe à l’utilisateur. Si le mot de passe est "javascript123", affiche "Accès autorisé", sinon affiche "Accès refusé".
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo 13: Convertisseur Celsius-Fahrenheit :
+// Crée une fonction qui prend une température en Celsius et la convertit en Fahrenheit. La formule est : F = C * 9/5 + 32. Utilise la fonction pour convertir une température et afficher le résultat.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo 14: Filtrer un tableau :
+// Déclare un tableau de nombres (par exemple, [2, 8, 5, 13, 4, 6, 7]). Utilise une boucle pour créer un nouveau tableau qui contient seulement les nombres supérieurs à 5, puis affiche ce nouveau tableau.
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Exo15:Écrire un programme qui demande à un utilisateur d’écrire son âge, et lui retourne sa catégorie d’âge.
+//  NB: 0   à   14 ans  => Enfants
+//      15  à   25 ans  => Adolescents
+//      25  à   64 ans  => Adultes
+//      65 ans et plus  => Aînés
